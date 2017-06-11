334fbd6b82d9793cbe008e5eb17d8536a544671a
Jesper Bränn <joppe@Jespers-MacBook-Pro.local>
2017-06-12 01:21:32 +0200

    Add FAQ

--- studsapp/FAQViewController.swift
+++ studsapp/FAQViewController.swift
@@ -10,18 +10,54 @@ import UIKit
 import Firebase
 
 class FAQViewController: UIViewController {
+    @IBOutlet var webView: UIWebView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
     var databaseRef: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
+        webView.isOpaque = false;
+        webView.backgroundColor = UIColor.clear
+        //webView.scalesPageToFit = true;
+        
+        databaseRef = Database.database().reference()
+        databaseRef.child("static").child("faq").observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as! String
+            self.webView.loadHTMLString(value, baseURL: nil)
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
 
+extension FAQViewController: UIWebViewDelegate {
+    func webViewDidFinishLoad(_ webView : UIWebView) {
+        activityIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        switch navigationType {
+        case .linkClicked:
+            // Open links in Safari
+            guard let url = request.url else { return true }
+            
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                // openURL(_:) is deprecated in iOS 10+.
+                UIApplication.shared.openURL(url)
+            }
+            return false
+        default:
+            // Handle other navigation types...
+            return true
+        }
+    }
+}
